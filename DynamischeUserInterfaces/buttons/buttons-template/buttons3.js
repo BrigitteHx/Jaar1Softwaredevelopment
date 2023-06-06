5d22e57c68b142f868b1528cc5249ced377bcfc7
brigitte <99073629@mydavinci.nl>
2023-06-06 15:25:11 +0200

    buttons3 stap 3 en stap 4

--- DynamischeUserInterfaces/buttons/buttons-template/buttons3.js
+++ DynamischeUserInterfaces/buttons/buttons-template/buttons3.js
@@ -13,8 +13,35 @@ function generateButtons(numButtons) {
       button.innerText = i;
       container.appendChild(button);
   
+      // klik op button
       button.addEventListener("click", function() {
-        this.style.backgroundColor = "red"; // knop naar rood
+        // HULP STACK OVERFLOW DATA KLEUR EN DATA KLIK OPHALEN: 
+        var currentColor = this.getAttribute("data-color"); 
+        var currentClicks = parseInt(this.getAttribute("data-clicks")) || 0; 
+  
+        // kleuren
+        if (currentClicks === 0) {
+          this.style.backgroundColor = "red";
+          this.setAttribute("data-color", "red");
+        } else if (currentClicks === 1) {
+          this.style.backgroundColor = "purple";
+          this.setAttribute("data-color", "purple");
+        } else if (currentClicks === 2) {
+          this.style.backgroundColor = "blue";
+          this.setAttribute("data-color", "blue");
+        } else if (currentClicks === 3) {
+          this.style.backgroundColor = "black";
+          this.setAttribute("data-color", "black");
+        }
+  
+        currentClicks++; 
+  
+        // is kleur zwart?
+        if (currentClicks === 5 && currentColor === "black") {
+          this.remove(); // remove uit DOM
+        } else {
+          this.setAttribute("data-clicks", currentClicks); 
+        }
       });
     }
   
